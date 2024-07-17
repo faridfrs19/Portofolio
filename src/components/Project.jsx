e4a05d18425c2f3ee29d0b44585ee17f7a95f796
faridfrs19 <faridgans129@gmail.com>
2024-07-17 07:04:56 +0700

    Fix warna text

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -75,14 +75,14 @@ const Project = () => {
               <CardBody className="bg-hero relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full sm:w-[30rem] h-auto rounded-xl p-6 border">
                 <CardItem
                   translateZ="50"
-                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                  className="text-xl font-bold text-white"
                 >
                   {project.name}
                 </CardItem>
                 <CardItem
                   as="p"
                   translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  className="text-slate-400 text-sm max-w-sm mt-2"
                 >
                   {project.description}
                 </CardItem>
